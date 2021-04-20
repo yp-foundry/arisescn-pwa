05d9f026311c458ae614d7ffdc669d845b5423fa
Opeyemi David <ayobamideleope79@gmail.com>
2021-04-20 15:48:22 +0100

    correct info

--- data/messages.js
+++ data/messages.js
@@ -882,7 +882,7 @@ export default [
     link:
       'https://a.storyblok.com/f/90098/x/8ee71263fd/the-mystery-of-salvation-1-olanrewaju-ilori-arise.mp3',
     minister: 'Olanrewaju Ilori',
-    date: new Date('April 10, 2021 19:30:00'),
+    date: new Date('April 17, 2021 19:30:00'),
     series: 'The Mystery of Salvation',
     imgSrc: `/img/album-arts/2021/The-Mystery-of-Salvation/The-Mystery-of-Salvation-1.jpg`,
     thumbnailSrc: `/img/album-arts/2021/The-Mystery-of-Salvation/The-Mystery-of-Salvation-1-thumbnail.jpg`
