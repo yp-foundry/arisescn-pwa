2db9826323bc4d1faf643f59f0e1e8336d9d4d6e
Opeyemi David <ayobamideleope79@gmail.com>
2021-06-03 15:57:36 +0100

    add message - Immortality

--- data/messages.js
+++ data/messages.js
@@ -946,6 +946,14 @@ export default [
     series: 'Abba Father',
     imgSrc: `/img/album-arts/2021/Abba-Father/Abba-Father-3-Arise.jpg`,
     thumbnailSrc: `/img/album-arts/2021/Abba-Father/Abba-Father-3-Arise-thumbnail.jpg`
+  },
+  {
+    title: `Immortality`,
+    link:
+      'https://a.storyblok.com/f/90098/x/a17b942c06/immortality-olanrewaju-ilori-arise.mp3',
+    minister: 'Olanrewaju Ilori',
+    date: new Date('May 29, 2021 19:30:00'),
+    series: '-'
   }
 ]
 
