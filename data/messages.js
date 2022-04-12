b9f1f543da96dab746d8386ce15a8245f829cdd4
Opeyemi David <ayobamideleope79@gmail.com>
2022-04-12 22:34:37 +0100

    content: add message

--- data/messages.js
+++ data/messages.js
@@ -1354,6 +1354,16 @@ export default [
     series: '-',
     imgSrc: `/img/album-arts/2022/Incense_and_Healing_(April_Edition)_-_Album_Art.jpg`,
     thumbnailSrc: `/img/album-arts/2022/Incense_and_Healing_(April_Edition)_-_Album_Art_-_thumbnail.jpg`
+  },
+  {
+    title: `The Believer's Authority 1`,
+    link:
+      'https://a.storyblok.com/f/90098/x/0c3c04ca1e/the-believer-s-authority-1-olanrewaju-ilori-arise.mp3',
+    minister: 'Olanrewaju Ilori',
+    date: new Date('April 2, 2022 19:30:00'),
+    series: '-',
+    imgSrc: `/img/album-arts/2022/The_Believer's_Authority/The_Believer's_Authority_1_-_Album_Art.jpg`,
+    thumbnailSrc: `/img/album-arts/2022/The_Believer's_Authority/The_Believer's_Authority_1_-_Album_Art_-_thumbnail.jpg`
   }
 ]
 
