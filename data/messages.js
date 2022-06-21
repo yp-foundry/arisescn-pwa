404fbe36f53411222d42234d5a3e7e6a1edc44f8
Opeyemi David <codescype@gmail.com>
2022-06-21 09:03:59 +0100

    content: add message

--- data/messages.js
+++ data/messages.js
@@ -1334,6 +1334,15 @@ export default [
     series: '-',
     imgSrc: `/img/album-arts/2022/Spiritual_Laws/The_Law_of_Sound_-_Album_Art.jpg`,
     thumbnailSrc: `/img/album-arts/2022/Spiritual_Laws/The_Law_of_Sound_-_Album_Art_-_thumbnail.jpg`
+  },
+  {
+    title: `The Law of Divine Direction`,
+    link: 'https://a.storyblok.com/f/90098/x/1a639e9dd1/the-law-of-divine-direction-olanrewaju-ilori-arise.mp3',
+    minister: 'Olanrewaju Ilori',
+    date: new Date('June 18, 2022 19:30:00'),
+    series: '-',
+    imgSrc: `/img/album-arts/2022/Spiritual_Laws/The_Law_of_Divine_Direction_-_Album_Art.jpg`,
+    thumbnailSrc: `/img/album-arts/2022/Spiritual_Laws/The_Law_of_Divine_Direction_-_Album_Art_-_thumbnail.jpg`
   }
 ]
 
